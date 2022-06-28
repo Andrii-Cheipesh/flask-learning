ee0d0d24178e15fca12806898030983c09bf938a
Dzintari <garri.andriy@gmail.com>
2022-06-28 17:10:23 +0300

    Added running server from file.

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello world!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hi, {name}!</h1>'
+
+
+if __name__ == __name__:
+    app.run(debug=True)
