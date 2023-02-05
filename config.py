f2c77bc80f5328cda75b4a057f0c2d2a0ff4dd4d
Andrii-Cheipesh <garri.andriy@gmail.com>
2023-02-05 13:34:24 +0200

    Config fix for prod

--- config.py
+++ config.py
@@ -21,7 +21,8 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
